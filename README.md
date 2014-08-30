b97b63f74a9016bf1e35147944b1c2da72fb9411
G3F4 <kamil.grabek@gmail.com>
2014-08-30 22:26:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FSDL
+
+	App framework.
+
+## License
+
+Copyright (C) 2014 G3F4
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int argc, char* args[] )
+{
+	std::cout << "ok" << std::endl;
+	return 0;
+}
+
